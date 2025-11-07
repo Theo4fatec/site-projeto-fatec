296b897822345f0f46b821eb12181e0e330451cd
theo <theo@site-fatec-projeto.com>
2025-11-07 18:58:19 -0300

    atualização2

--- site-projeto-fatec/urls.py
+++ site-projeto-fatec/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('colab/', include('colab.urls')),
-    path('', include('pagina_inicial.urls'))
+    path('', include('pagina_inicial.urls')),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
